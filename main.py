111bf54eadbca661bd171dfc7eff071c95d3deff
Matheus Karrielle <109245233+Karrielle@users.noreply.github.com>
2024-10-19 15:21:20 -0300

    add_menu

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
-
-
+from kivy.core.window import Window
 
 class Gerenciador(ScreenManager):
+    pass
+
+class Menu(Screen):
     pass
 
 class Tarefas(Screen):
@@ -12,23 +14,33 @@ class Tarefas(Screen):
         super().__init__(**kwargs)
         for tarefa in tarefas:
             self.ids.box.add_widget(Tarefa(text=tarefa))  # Adiciona tarefas iniciais
+    
+    def on_pre_enter(self):
+        Window.bind(on_keyboard=self.voltar)
+
+    def voltar(self,window,key, *args):
+        if key == 27:
+            App.get_running_app().root.current = 'menu'
+            return True
+    
+    def on_pre_leave(self):
+        Window.unbind(on_keyboard=self.voltar)
+        
+
 
     def add_widget_tarefa(self):
-        texto = self.ids.texto.text.strip()  # Remove espaços desnecessários
-        if texto:  # Adiciona apenas se houver texto
+        texto = self.ids.texto.text.strip()  # Remove espaços em branco
+        if texto:
             self.ids.box.add_widget(Tarefa(text=texto))
             self.ids.texto.text = ''  # Limpa o campo de texto
-
 
 class Tarefa(BoxLayout):
     def __init__(self, text='', **kwargs):
         super().__init__(**kwargs)
         self.ids.label.text = text  # Define o texto na label
 
-
 class Test(App):
     def build(self):
         return Gerenciador()
-
 
 Test().run()
